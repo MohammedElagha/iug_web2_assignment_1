1a97f648392a4da4754fd3d4175f02718e83d11d
Emad A. Abu Lebda <emadexodus@gmail.com>
2019-03-02 18:21:43 +0200

    Create assignment1.php
    120170588 Assignment 1

---
+++ 120170588/assignment1.php
@@ -0,0 +1,13 @@
+<?php
+        /*
+        Create an associative array, the Indices are the titles of your registered
+         courses last semester and the values are your grades. 
+        */
+    $grades = array(
+        "Data Stracture" =>88,
+        "Web Development 1"=> 90,
+        "Mathematical Computing" =>80,
+        "Software Engineering"=>85
+    );
+    print_r($grades);
+?>
